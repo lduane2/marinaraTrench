de1e13cae1b69c8e758544cba4effd308cc56316
Nick Carroll <ncarroll@nd.edu>
2017-05-03 17:44:27 -0400

    added comments

--- client.py
+++ client.py
@@ -15,9 +15,14 @@ import random
 
 import urllib
 
+
+#these are the imports for pyglet
+
 import pyglet
 import pygletreactor
 pygletreactor.install()
+
+#these are the imports for cocos
 
 import cocos
 from cocos.scene import Scene
@@ -29,9 +34,12 @@ from cocos.director import director
 from cocos.scenes import FadeTransition
 from cocos.text import Label
 
+#these are the imports for pyglet window
 
 from pyglet.window import key
 from pyglet.window.key import symbol_string
+
+#these are the imports for twisted
 
 from twisted.protocols import basic
 from twisted.internet import reactor, protocol
@@ -45,11 +53,16 @@ import math
 import sys
 import json
 
+
+#global variable declararions
+
 WINDOW_WIDTH = 700;
 WINDOW_HEIGHT = 500;
 OFFSET = 60
 SPEED = 200
 color = (52, 152, 219)
+
+
 
 SPRITE_POS = '';
 keys = ''
@@ -72,47 +85,47 @@ IDENTIFIER = ''
 
 spaghetti_names = ["spaghetti0", "spaghetti1", "spaghetti2", "spaghetti3", "spaghetti4", "spaghetti5", "spaghetti6", "spaghetti7", "spaghetti8", "spaghetti9"]
 
-class MoveSubmarine(Move):
+class MoveSubmarine(Move):  # the movement of the subamrine
     global SPRITE_POS
 
     sprite_data = {}
     def step(self, dt):
         global SPRITE_POS
         global connection
-        if keys[key.RIGHT]:
+        if keys[key.RIGHT]:  # if the right key is down 
             #print ("RIGHT")
-            new_pos = [self.target.position[0] + 3, self.target.position[1]]
+            new_pos = [self.target.position[0] + 3, self.target.position[1]]   # creat new position
             if new_pos[0] < WINDOW_WIDTH:
-                self.target.position = tuple(new_pos)
+                self.target.position = tuple(new_pos)  # move to new position if in bounds
             try:
-                self.ping()
+                self.ping()  # send location to servr
             except:
                 pass
         if keys[key.LEFT]:
             #print ("LEFT")
-            new_pos = [self.target.position[0] - 3, self.target.position[1]]
+            new_pos = [self.target.position[0] - 3, self.target.position[1]] # creat new position
             if new_pos[0] > 0:
-                self.target.position = tuple(new_pos)
+                self.target.position = tuple(new_pos)  # move to new position if in bounds
             try:
-                self.ping()
+                self.ping()  # send location to servr
             except:
                 pass
 	if keys[key.UP]:
             #print ("UP")
-            new_pos = [self.target.position[0], self.target.position[1] + 3]
+            new_pos = [self.target.position[0], self.target.position[1] + 3]   # creat new position
             if new_pos[1] < WINDOW_HEIGHT:
-                self.target.position = tuple(new_pos)	
+                self.target.position = tuple(new_pos)	 # move to new position if in bounds
             try:
-                self.ping()
+                self.ping()  # send location to servr
             except:
                 pass
         if keys[key.DOWN]:
             #print ("DOWN")
-            new_pos = [self.target.position[0], self.target.position[1] - 3]
+            new_pos = [self.target.position[0], self.target.position[1] - 3]   # creat new position
             if new_pos[1] > 0:
-                self.target.position = tuple(new_pos)
+                self.target.position = tuple(new_pos)  # move to new position if in bounds
             try:
-	        self.ping()
+	        self.ping()  # send location to servr
             except:
                 pass
 	#print (self.target.sprite_data)
@@ -129,16 +142,16 @@ class MoveSubmarine(Move):
         #print(json.dumps(self.sprite_data))
         connection.transport.write(json.dumps(self.sprite_data) + '\n')
 
-class MoveBubbles(Move):
+class MoveBubbles(Move):  # moves the bubles
     def step(self, dt):
-        if self.target.position[1] > WINDOW_HEIGHT:
-            self.target.position = random.randint(10, WINDOW_WIDTH -1), -20
-        self.target.velocity = (1 , 20 )
-        super(MoveBubbles, self).step(dt)
+        if self.target.position[1] > WINDOW_HEIGHT:  # if the bubble is outside the view of the screen
+            self.target.position = random.randint(10, WINDOW_WIDTH -1), -20  # move it to a random position beow the screen
+        self.target.velocity = (1 , 20 )  #sets the velocity
+        super(MoveBubbles, self).step(dt) # makes it do it again
 
         
 class MoveSpaghetti(Move):
-    def step(self, dt):
+    def step(self, dt):  # helps with the movement of the spaghetti
         global SPRITE_POS
         global score
 
@@ -160,37 +173,38 @@ class MoveSpaghetti(Move):
 
 
         
-        self.target.velocity = (0, 0)
-        super(MoveSpaghetti, self).step(dt)
+        self.target.velocity = (0, 0)  # sets the velocity equal to zero
+        super(MoveSpaghetti, self).step(dt) 
 
 
 
-class Actions(ColorLayer):
+class Actions(ColorLayer):  # represents the actions taht can take place on the main layer
     is_event_handler = True
     sprite_vector = {}
     sprite_data = {}
 
-    def step(self, dt):
+    def step(self, dt):   #testing the step fnciton
 	print ("pease")
 
-    def __init__(self):
+    def __init__(self): # initialze
         super(Actions, self).__init__(52, 152, 219, 1000)
 
 	beatlesMusic = pyglet.media.load('yelloSub.wav', streaming=False)
-	beatlesMusic.play()
-        self.sprite = Sprite('sub1.png')
+	beatlesMusic.play()  # load and play the music
+        self.sprite = Sprite('sub1.png')  # load and establish the sprite
         self.sprite.position = OFFSET, WINDOW_HEIGHT - OFFSET
         self.sprite.velocity = 0, 0
         self.add(self.sprite)
-        self.spaghetti = {}
-        self.make_spaghetti()
+        self.spaghetti = {} 
+        self.make_spaghetti() # make the spagetti
         self.bubbles = {}
-        self.make_bubbles()
+        self.make_bubbles()  # make the bubbles
         #making the seaweed
         self.seaweed = Sprite('seaweed.png')
         self.seaweed.position = WINDOW_WIDTH / 2 - 15, 0
         self.add(self.seaweed)
         
+        #set up the subs
         self.sprite_vector["client0"] = self.sprite
         self.sprite_vector["client1"] = self.sprite
         self.sprite.do(MoveSubmarine())
@@ -205,17 +219,18 @@ class Actions(ColorLayer):
         self.scores.position = 50, WINDOW_HEIGHT - 20
         self.add(self.scores)
 
+        #set the update loop
         self.updateLoop = LoopingCall(self.update_client)
         self.updateLoop.start(1.0)
 
 
-    def on_key_press(self, keyPress, modifiers):
+    def on_key_press(self, keyPress, modifiers):  # when input is recieved from the keyboard
         global SPEED
         global IDENTIFIER
         global connection
         global keys
 
-        while keys[key.RIGHT]:
+        while keys[key.RIGHT]:   #this ia how we used to program the movement of the sub, but the updated movement is now above
             print ("key is down")
             new_pos = [self.sprite.position[0] + 10, self.sprite.position[1]]
             if new_pos[0] < WINDOW_WIDTH:
@@ -224,7 +239,7 @@ class Actions(ColorLayer):
             break
 
     
-    def update_client(self):
+    def update_client(self):  
         '''updates global variables'''
         global color, timeLeft, p1_score, p2_score
         #updates color
@@ -251,43 +266,43 @@ class Actions(ColorLayer):
         self.add(self.scores)
     
 
-    def echo(self):
+    def echo(self): # another test function
         print('Hello')
 
-    def make_spaghetti(self):
+    def make_spaghetti(self):  # initializes the spagett
         global spaghetti_names
 
         #print('\ntryna cook up some spaghetti\n')
-        for i in spaghetti_names:
+        for i in spaghetti_names:  # place all of the spagetti
             #print(self.spaghetti)
             random.seed()
             self.new_sprite = Sprite('spaghetti.png')
             #self.new_sprite.position = random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)
             self.new_sprite.position = -100, -100
             #self.new_sprite.velocity = random.randint(-30, 30), random.randint(20, 40)
-            self.new_sprite.do(MoveSpaghetti())
+            self.new_sprite.do(MoveSpaghetti())  # move the spagetti
             self.add(self.new_sprite)
-            self.spaghetti[i] = self.new_sprite
+            self.spaghetti[i] = self.new_sprite # add the spagetti to the array of sprites
 
-    def make_bubbles(self):
+    def make_bubbles(self):  # makes bubbles
 	bubbleCount = 10
-        for i in range(bubbleCount):
+        for i in range(bubbleCount): # make a specific amount of bubbles
             random.seed()
             self.new_sprite = Sprite('bubb.png')
-            self.new_sprite.position = random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)
+            self.new_sprite.position = random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20) #  set position
             #self.new_sprite.position = -100, -100
-            self.new_sprite.velocity = random.randint(-30, 30), 1
+            self.new_sprite.velocity = random.randint(-30, 30), 1  # set velocity of the bubbles
             self.new_sprite.do(MoveBubbles())
             self.add(self.new_sprite)
-            self.bubbles[i] = self.new_sprite
+            self.bubbles[i] = self.new_sprite  # add them to the array
 
-    def add_sprite(self, num):
-        sprite_add = Sprite('sub2.png')
-        sprite_add.position = -100, -100
+    def add_sprite(self, num):  # add the oponent sub
+        sprite_add = Sprite('sub2.png') # 
+        sprite_add.position = -100, -100 # set its positions
         self.add(sprite_add)
-        self.sprite_vector[num] = sprite_add
+        self.sprite_vector[num] = sprite_add  # add the sprite to the array 
 
-    def increment_sprites(self, packet):
+    def increment_sprites(self, packet):  # changes the position of the sprites
         global transition
         global spaghetti_names
         global IDENTIFIER
@@ -296,70 +311,70 @@ class Actions(ColorLayer):
 
         #print("ID", IDENTIFIER)
 
-        self.packet_dict = json.loads(packet)
-        timeLeft = self.packet_dict['time']
+        self.packet_dict = json.loads(packet)  # receieve the json from the server
+        timeLeft = self.packet_dict['time']   #unpack it and edit the global gamestate
         p1_score = self.packet_dict['p1_score']
         p2_score = self.packet_dict['p2_score']
-        if int(timeLeft) == 0 and not transition:
+        if int(timeLeft) == 0 and not transition:  #  if the game is over
             #print ("Negative")
             transition = True
             timeLeft = -1            
             #self.updateLoop.stop()
-            director.replace(FadeTransition(Scene(endLayer())))
+            director.replace(FadeTransition(Scene(endLayer())))  # transition to the game over screen 
             #reactor.stop()
-        if int(timeLeft) < 0:
+        if int(timeLeft) < 0:  # stop looping if the time is negative
             return        
 #self.updateLoop.end()
         #print(self.packet_dict)   
-        for el in self.packet_dict:
+        for el in self.packet_dict:  # use the information from the server to update the position of the spagetti
             if el != IDENTIFIER and el in ["client0", "client1"]:
                 #print(el)
                 self.sprite_vector[el].position = tuple(self.packet_dict[el]['position'])
             if el in spaghetti_names:
                 self.spaghetti[el].position = tuple(self.packet_dict[el]['position'])
         
-    def ping(self):
+    def ping(self):  # send data to the server
         #print("PING")
-        self.sprite_data["ID"] = IDENTIFIER
-        self.sprite_data["position"] = self.sprite.position
+        self.sprite_data["ID"] = IDENTIFIER  # send which player you are
+        self.sprite_data["position"] = self.sprite.position  # send your position
         #print(json.dumps(self.sprite_data))
-        connection.transport.write(json.dumps(self.sprite_data) + '\n')
+        connection.transport.write(json.dumps(self.sprite_data) + '\n') # send it across the web
 
 
 # a client protocol
-class MarinaraClient(basic.LineReceiver):
+class MarinaraClient(basic.LineReceiver):  # inherent the client from the basic line receiver class
     global act
     global IDENTIFIER
     m_INIT = True
     num_clients = 11;
 
     """Once connected, send a message, then print the result."""
-    def connectionMade(self):
+    def connectionMade(self):  # if the connection is successful
         global connection
 
         self.delimiter = '\n'
         connection = self
 
-        print('Connection Made')
+        print('Connection Made')  # let the user know the connection was successful
 
-    def lineReceived(self, data):
+    def lineReceived(self, data):  # when you recieve a line formt he server
         global IDENTIFIER
 
-        if self.m_INIT:
+        if self.m_INIT: 
             #print("ID", str(data).rstrip())
-            IDENTIFIER = str(data).rstrip()
+            IDENTIFIER = str(data).rstrip()  # initialize yourself
 
             self.m_INIT = False
         else:
-            self.add_client(data)
+            self.add_client(data) # otherwise add the second client data
 
-    def add_client(self, data):
+    def add_client(self, data): # add the second client 
         global IDENTIFIER
 
         packet = str(data).rstrip()
-        packet_dict = json.loads(packet)
+        packet_dict = json.loads(packet) # load the pack from the server
         if len(packet_dict) > self.num_clients:
-            for el in packet_dict:
+            for el in packet_dict:  # unpack the information form the server
                 if el != IDENTIFIER and el in ["client0", "client1"]:
                     act.add_sprite(el)
                     self.num_clients += 1
@@ -368,55 +383,55 @@ class MarinaraClient(basic.LineReceiver):
         else:
             act.increment_sprites(packet)
 
-    def connectionLost(self, reason):
+    def connectionLost(self, reason):  # report errors to command line
         print("connection lost")
 
  
-class EchoFactory(protocol.ClientFactory):
+class EchoFactory(protocol.ClientFactory):  # the echo factory client 
     protocol = MarinaraClient
 
-    def clientConnectionFailed(self, connector, reason):
-        print("Connection failed - goodbye!")
+    def clientConnectionFailed(self, connector, reason):  # if the connection fails
+        print("Connection failed - goodbye!")     # print an error message and end the program 
         reactor.stop()
     
-    def clientConnectionLost(self, connector, reason):
-        print("Connection lost - goodbye!")
+    def clientConnectionLost(self, connector, reason): # if the connection is lost
+        print("Connection lost - goodbye!")   # print an error message and stop the program
 
 
         reactor.stop()
 
 
-class endLayer(ColorLayer):
+class endLayer(ColorLayer):   # the layer for the end of game screen 
     is_event_handler =True
     
     def __init__(self):
-        global p2_score
+        global p2_score # accesses the score for both players to display it in the final message
         global p1_score
-        super(endLayer, self).__init__(52, 152, 219, 1000)    
+        super(endLayer, self).__init__(52, 152, 219, 1000)    # initialize this layer with the same conditions as the previous one
         #text = Label("You Won!")
-        print (IDENTIFIER)
+        print (IDENTIFIER)  #print diagnostic info so we know which player this is
         score = 0        
         opScore = -1
-        if IDENTIFIER == "client1":
+        if IDENTIFIER == "client1": # if you are the first, assign the score appropraitely 
             score = p2_score
             opScore = p1_score
-	else:
+	else:  # otherwise, flip flop the score values
             score = p1_score
 	    opScore = p2_score
-        message = "You Lost.  Try Again Next Time!"
+        message = "You Lost.  Try Again Next Time!"  # set the losing message
         if score > opScore:
-            message = "You Won, Congrats!"
+            message = "You Won, Congrats!"  # but overwrite it if your score was better
         
         
         #print ("we did it patrick, we saved the city")
-        text = Label(message)
-	text2 = Label("Your Score: " +str(score) + " Opponent Score: " + str(opScore) )
+        text = Label(message) # create a label with your victory / loss message
+	text2 = Label("Your Score: " +str(score) + " Opponent Score: " + str(opScore) ) # create another label with the player scores
 
-        text2.position = director._window_virtual_width / 4, (director._window_virtual_height / 2) -30
+        text2.position = director._window_virtual_width / 4, (director._window_virtual_height / 2) -30 #place the two different text labels
 
         text.position = director._window_virtual_width / 4, director._window_virtual_height / 2
 
-        self.add(text)
+        self.add(text) # add the text to the scenen
         self.add(text2)
 
 
@@ -427,26 +442,26 @@ class endLayer(ColorLayer):
 def main():
     global act
     global keys
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 3: # parses the arguments 
         print("usage: python client.py <server address> <port>")
         exit(1)
-    director.init(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
-    keys = key.KeyStateHandler()
-    cocos.director.director.window.push_handlers(keys)
-    act = Actions()
-    main_scene = cocos.scene.Scene(act)
+    director.init(width=WINDOW_WIDTH, height=WINDOW_HEIGHT) # initialoize the director
+    keys = key.KeyStateHandler() # store the keyboard state to allow movment 
+    cocos.director.director.window.push_handlers(keys) # let the window understand the keystate
+    act = Actions() 
+    main_scene = cocos.scene.Scene(act) # initialize and push the scene 
 
     @director.window.event
-    def on_close():
+    def on_close(): #stop the reactor when the window closes
         reactor.stop()
 
-    director.scene_stack.append(director.scene)
+    director.scene_stack.append(director.scene) # set up the first scene
     director._set_scene(main_scene)   
 
-    f = EchoFactory()
+    f = EchoFactory() # set up the networking
     reactor.connectTCP(sys.argv[1], int(sys.argv[2]), f)
-    reactor.run()
+    reactor.run()  # begin the reacotr
 
 # this only runs if the module was *not* imported
-if __name__ == '__main__':
+if __name__ == '__main__': # starts the client
     main()
--- server.py
+++ server.py
@@ -34,51 +34,51 @@ class ChatClient(basic.LineReceiver):
     #spaghetti0 spaghetti1 spaghetti2 ... spaghettiN in the dictionary
     
 
-    def init(self):
+    def init(self):  # set up the server
         global spaghetti_names
 
-        for spa in spaghetti_names:
+        for spa in spaghetti_names:  # store the information about the location of the spagetti
             self.client_dict[spa] = {"position": [random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)]}
 
-        self.client_dict['time'] = timeLeft
-        self.client_dict['p1_score'] = p1_score
+        self.client_dict['time'] = timeLeft  # add time to the diction
+        self.client_dict['p1_score'] = p1_score # add the scores to the dictionary
         self.client_dict['p2_score'] = p2_score
 
-        self.lc = LoopingCall(self.send)
+        self.lc = LoopingCall(self.send)  # start the looping cal
         self.lc.start(1.0 / FPS)
 
-    def connectionMade(self):
+    def connectionMade(self):  # when a connection is made
         global identifier
         print("Got new client!")
         self.delimiter = '\n'
-        self.factory.clients.append(self)
+        self.factory.clients.append(self)   
 
-        if identifier < 2:
+        if identifier < 2:    # save the information for the first two people who connect, they are the players
             id_message = client_names[identifier]
             self.message(id_message)
             self.client_dict[client_names[identifier]] = {"position": [60,440]}
             identifier += 1
 
-        if identifier is 1:
+        if identifier is 1:  # start the game when both players have connected
             self.init()
 
-    def connectionLost(self, reason):
+    def connectionLost(self, reason):  # when the connection is gone
         print("Lost a client!")
-        self.factory.clients.remove(self)
+        self.factory.clients.remove(self)  #remove
 
-    def lineReceived(self, line):
+    def lineReceived(self, line):  # when we recieve information from the client
         json_receive = json.loads(line.rstrip())
-        self.client_dict[json_receive["ID"]] = {"position": json_receive["position"]}
+        self.client_dict[json_receive["ID"]] = {"position": json_receive["position"]}# add it to the dictionar for the next send
         #print self.client_dict
 
-    def send(self):
+    def send(self):  #
         global timeLeft, p1_score, p2_score
         #collision detection
-        if 'client0' in self.client_dict.keys() and 'client1' in self.client_dict.keys():
+        if 'client0' in self.client_dict.keys() and 'client1' in self.client_dict.keys():  # if you have both players...
             p1_pos = self.client_dict['client0']['position']
-            p2_pos = self.client_dict['client1']['position']
+            p2_pos = self.client_dict['client1']['position']   # save the positiom
             for spa in spaghetti_names:
-                spa_pos = self.client_dict[spa]['position']
+                spa_pos = self.client_dict[spa]['position']   # compare the position of the player with the position of the spagetti  If they they overlap, add to thecorrect score
                 if p1_pos[0] > spa_pos[0] - 31 and p1_pos[0] < spa_pos[0] + 31 and p1_pos[1] > spa_pos[1] - 31 and p1_pos[1] < spa_pos[1] + 31: 
                     p1_score += 1
                     self.client_dict[spa] = {"position": [random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)]}
@@ -86,9 +86,9 @@ class ChatClient(basic.LineReceiver):
                     p2_score += 1
                     self.client_dict[spa] = {"position": [random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)]}
                 if spa_pos[1] > WINDOW_HEIGHT:
-                    self.client_dict[spa] = {"position": [random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)]}
+                    self.client_dict[spa] = {"position": [random.randint(10, WINDOW_WIDTH - 10), random.randint(-500, -20)]} # if the spagetti goes too high, spawn it back below the screen
             try:
-                self.client_dict['p1_score'] = p1_score
+                self.client_dict['p1_score'] = p1_score # update the scores back to the other player
                 self.client_dict['p2_score'] = p2_score
             except:
                 pass
@@ -104,16 +104,16 @@ class ChatClient(basic.LineReceiver):
             c.message(json.dumps(self.client_dict))
 
 
-    def message(self, message):
+    def message(self, message):  # send the info
         self.transport.write(message + '\n')
 
 def main():
-    f = Factory()
-    f.protocol = ChatClient
+    f = Factory() # start the factory
+    f.protocol = ChatClient  #start the chat client
     f.clients = []
     print('Listening on 8000...')
-    reactor.listenTCP(8000, f)
-    reactor.run()
+    reactor.listenTCP(8000, f)  # listen on the port
+    reactor.run() # run the reactor
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # initialize the application
     main()
